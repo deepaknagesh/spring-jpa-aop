1b0824a31716e21eaa4126f0b69c8859486d4cab
Manish Baxi <baxi.manish@welue.com>
2015-03-02 07:51:44 +0530

    Updated aspect to log class and method name for intercepted methods.

--- src/main/java/org/example/aop/InterceptionAdvice.java
+++ src/main/java/org/example/aop/InterceptionAdvice.java
@@ -1,5 +1,6 @@
 package org.example.aop;
 
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.slf4j.LoggerFactory;
@@ -10,8 +11,10 @@ import org.springframework.stereotype.Component;
 public class InterceptionAdvice
 {
   @Before("execution(* org.example.data.*Repository+.findByName(..))")
-  public void intercept()
+  public void intercept(JoinPoint joinPoint)
   {
-    LoggerFactory.getLogger(getClass()).info("A method was intercepted.");
+    LoggerFactory.getLogger(getClass()).warn(String.format("The method %s::%s was intercepted."
+        , joinPoint.getSignature().getDeclaringTypeName()
+        , joinPoint.getSignature().getName()));
   }
 }
