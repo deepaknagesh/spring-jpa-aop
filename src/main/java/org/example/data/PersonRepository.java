f8d7631bb1e6b0bb3dd6a1dad66a9d8701577b8e
Manish Baxi <baxi.manish@welue.com>
2015-03-02 07:41:12 +0530

    Removed annotation not used any more.

--- src/main/java/org/example/data/PersonRepository.java
+++ src/main/java/org/example/data/PersonRepository.java
@@ -1,11 +1,9 @@
 package org.example.data;
 
-import org.example.aop.Intercepted;
 import org.example.domain.Person;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PersonRepository extends JpaRepository<Person, Long>
 {
-  @Intercepted
   Person findByName(String name);
 }
